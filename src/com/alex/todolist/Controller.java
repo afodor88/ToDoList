8c5d42208fca1acb3d7d45f3f7cdd0d7c5a283c6
afodor88 <afodor88@gmail.com>
2022-02-06 17:25:19 +0200

    adding a toolbar with a New button that has an icon and a tooltip;

--- src/com/alex/todolist/Controller.java
+++ src/com/alex/todolist/Controller.java
@@ -10,6 +10,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.util.Callback;
@@ -140,6 +142,16 @@ public class Controller {
             DialogController controller = fxmlLoader.getController();
             TodoItem newItem = controller.processResults();
             todoListView.getSelectionModel().select(newItem);
+        }
+    }
+
+    @FXML
+    public void handleKeyPressed(KeyEvent keyEvent){
+        TodoItem selectedItem = todoListView.getSelectionModel().getSelectedItem();
+        if(selectedItem != null){
+            if(keyEvent.getCode().equals(KeyCode.DELETE)){
+                deleteItem(selectedItem);
+            }
         }
     }
 
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module JavaFxApplication {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jlfgr;
 
     opens com.alex.todolist;
 }
