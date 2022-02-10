7ae6e9198a8564faa84e877a2ad7b5a78bf99bb2
afodor88 <afodor88@gmail.com>
2022-02-10 19:51:54 +0200

    adding documentation of how to fix the error when the Java look and feel Graphics Repository jar file is missing;

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module JavaFxApplication {
     requires javafx.controls;
     requires javafx.fxml;
+    //if below red, download Java look and feel Graphics Repository https://www.oracle.com/java/technologies/java-look-and-feel-graphics-repository.html
     requires jlfgr;
 
     opens com.alex.todolist;
